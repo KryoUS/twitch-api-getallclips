34e70f86a5bdc2ed822a80b89c6367e04184bb3f
Michael Hughes <TaurusSHO89@gmail.com>
2020-11-01 02:27:35 -0700

    Added data collection and write to JSON. Added maximum clip response warning. Made stopping point more readible. Cleaned up loop logging.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const rateLimit = require('axios-rate-limit');
 const moment = require('moment');
+const fs = require('fs');
 
 //Add your Twitch ClientID and ClientSecret
 const clientId = '';
@@ -17,6 +18,39 @@ const api = rateLimit(axios.create(), { maxRPS: 13 });
 let clipCount = 0;
 let callCount = 0;
 
+//Data collection
+let clips = {
+    data: []
+};
+
+//Function to handle writing to JSON as well as duplicate checking
+const writeJSON = () => {
+
+    //Duplicate checking function that returns true or false
+    const checkDupes = () => {
+        let clipsArr = clips.data.map(obj => {
+            return obj.id;
+        });
+        return clipsArr.some((item, index) => {
+            return clipsArr.indexOf(item) != index;
+        });
+    };
+
+    //Convert clips to human-friendly JSON
+    let json = JSON.stringify(clips, null, 4);
+    fs.writeFile(`./data/clips.json`, json, 'utf8', (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (checkDupes(clips.data)) {
+                console.log(`-------------------------------WARNING: FILE WRITTEN BUT DUPLICATES WERE FOUND-------------------------------`);
+            } else {
+                console.log(`All clips have been written to file. Collection complete.`);
+            }            
+        }
+    });
+}
+
 //Create function for looping our API calls after we have a token
 function twitchClips(token, endTime) {
 
@@ -30,22 +64,46 @@ function twitchClips(token, endTime) {
             'Client-Id': clientId,
             Authorization: `Bearer ${token}`
         }
-    }).then(clips => {
-        
-        //Get current API Call Count and add 1.
-        callCount = callCount + 1;
-        //Get current Clips Count and add the length of Clips in the array.
-        clipCount = clipCount + clips.data.data.length;
-        
-        //If the date is May 24, 2016 then we can stop making calls. (Clips started on Twitch on the 25th)
-        //NOTE: Moment indexs months like an array, starting with January as 0
-        if (moment(endTime).year() === 2016 && moment(endTime).month() === 4 && moment(endTime).day() === 24) {
-            console.log('Done!');
-        } else {
-            //Log results and use this exact same function to get the next set of results
-            console.log('Calls:', callCount, '\t Total Clips:', clipCount, '\t End:', endTime, '\t Start:', startTime, '\t Clips Added:', clips.data.data.length);
-            twitchClips(token, startTime);
-        }
+    }).then(clipsRes => {
+
+        //Check for a response with 98 clips in it, if this happens we are likely to have some responses that are higher than the 100 hard cap
+        if (clipsRes.data.data.length >= 98) {
+            console.log(`-------------------------------MAXIMUM CLIP RETURN! (${clipsRes.data.data.length}) ENDING PROCESS-------------------------------`);
+            console.log(`--------------------------------Suggest lowering the 'hours' of Start Time and try running again--------------------------------`);
+            //End looping as we have a very high chance of not getting all the clips in the response
+            return;
+        };
+
+        //Set current Month and Year
+        //NOTE: MomentJS stores months in an array index, so we need to add one
+        let currMonth = moment(startTime).month() + 1;
+        let currYear = moment(startTime).year();
+
+        //Add data to collection
+        const data = clipsRes.data.data.map(clipsObj => {
+            clips.data.push(clipsObj);
+        });
+
+        //Use promise to ensure that the next API call waits until all the clips have been pushed to the collection array
+        Promise.all(data).then(() => {
+          
+            //Get current API Call Count and add 1.
+            callCount = callCount + 1;
+            //Get current Clips Count and add the length of Clips in the array.
+            clipCount = clipCount + clipsRes.data.data.length;
+
+            //If the date is May 24, 2016 then we can stop making calls. (Clips started on Twitch on the 25th)
+            if (currYear === 2016 && currMonth === 5 && moment(endTime).date() === 24) {
+                console.log('----------------Done! Writing array to file.----------------');
+                writeJSON(currYear, currMonth);
+            } else {
+                //Log results and use this exact same function to get the next set of results
+                console.log('Requests:', callCount, '\t Clips:', clipCount, '\t Added:', clipsRes.data.data.length, '\t Start:', startTime, '\t End:', endTime);
+                twitchClips(token, startTime);
+            }
+        }).catch(err => {
+            console.log(err);
+        });
 
     }).catch(err => {
         console.log(err)
